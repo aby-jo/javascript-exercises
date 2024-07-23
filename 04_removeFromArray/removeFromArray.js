8679efa5ace55ecec10af466be42279110068d42
aby-jo <173944472+aby-jo@users.noreply.github.com>
2024-07-23 21:05:52 +0530

    Finished the 4th excercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...nums) {
+    let index=[];
+
+    
+    for(let i=0;i<array.length;i++){
+        for(let j=0;j<nums.length;j++){
+            
+            if (array[i]===nums[j]){
+                index.push(i);
+            }
+        }
+    }
+    index.sort((a, b) => b - a);
+    for(let i=0;i<index.length;i++){ 
+        array.splice(index[i],1);
+    }
+    return array;
 };
 
 // Do not edit below this line
