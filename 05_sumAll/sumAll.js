acf76a26d9c7b361c8fd95d1381f05f24005ea67
aby-jo <173944472+aby-jo@users.noreply.github.com>
2024-07-23 23:28:12 +0530

    Finished the 5th excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(lowerbound,upperbound) {
+    let sum=0
+    let temp=0
+    if((lowerbound<0||typeof(lowerbound)!="number"||!Number.isInteger(lowerbound))||(upperbound<0)||typeof(upperbound)!="number"||!Number.isInteger(upperbound)){
+        sum="ERROR"}
+    else{
+    if(lowerbound>upperbound){
+        temp=upperbound
+        upperbound=lowerbound
+        lowerbound=temp
+    }
+    for(let i=lowerbound;i<=upperbound;i++){
+        sum+=i
+    }
+    }
+    return sum
 
 };
 
