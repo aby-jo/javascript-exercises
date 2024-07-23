dc70ab5406ff4ed3382547639e2ff591cf3fefcb
aby-jo <173944472+aby-jo@users.noreply.github.com>
2024-07-23 23:43:19 +0530

    Completed 6th excercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let output=false
 
+    if(year%4===0 && year%100!==0){
+        output=true
+    }
+    else if(year%400===0){
+        output=true
+    }
+
+    return output
 };
 
 // Do not edit below this line
