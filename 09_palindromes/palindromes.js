31c78f31e2c203aa69d8b21dbb3b0f0b4cbfa705
aby-jo <173944472+aby-jo@users.noreply.github.com>
2024-07-29 01:02:09 +0530

    Completed the 9th excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,31 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word=word.toLowerCase()
+    let split=word.split("")
+    let nospace=split.filter((item)=>{if(item==" "){
+        return false
+    }
+    else if(item==","){
+        return false
+    }
+    else if(item=="."){
+        return false
+    }
+      else if(item=="!"){
+        return false
+    }
+    else{
+        return true
+    }})
+    let rev=nospace.join()
+    nospace.reverse()
+    nospace=nospace.join()
+    for(let i=0;i<rev.length;i++){
+        if(rev[i]!=nospace[i]){
+            return false
+        }
+    }
+    return true
+
 
 };
 
