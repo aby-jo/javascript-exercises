1d11398a73f0e5b200e4526c6490cdd98744656c
aby-jo <173944472+aby-jo@users.noreply.github.com>
2024-07-29 02:08:37 +0530

    Completed the 12th excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(items) {
+let arr=items.reduce((initial,obj)=>{
+    if(obj.yearOfDeath==null){
+        currentdate=new Date()
+        currentyear=currentdate.getFullYear()
+        obj.yearOfDeath=currentyear
+    }
+    dif=(obj.yearOfDeath-obj.yearOfBirth)
+    return Math.max(dif,initial)
+},0)
+let ans=items.filter((obj)=>{
+    dif=(obj.yearOfDeath-obj.yearOfBirth)
+    if(dif==arr){
+        return true
+    }
+})
+return ans[0]
 };
 
 // Do not edit below this line
