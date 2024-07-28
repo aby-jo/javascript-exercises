8625db1b436f09761dd48b516f93e89a98fd3fdd
aby-jo <173944472+aby-jo@users.noreply.github.com>
2024-07-29 01:16:24 +0530

    Completed 10th excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let f=0
+    let s=1
+    if(num<0){
+        return("OOPS")
+    }
+    if(num==0){
+        return 0
+    }
+    if(num==1){
+        return 1
+    }
+    for(let i=0;i<num-1;i++){
+        fib=s+f
+        f=s
+        s=fib
+        
+    }
+    return fib
 };
 
 // Do not edit below this line
